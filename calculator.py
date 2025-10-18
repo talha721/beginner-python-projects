b6b867f4dba8fd463cf1f56aac5d58acd393f2df
talha721 <talhamoazzam4118@gmail.com>
2025-10-19 01:50:59 +0500

    Refactor calculator program to use functions for arithmetic operations

--- calculator.py
+++ calculator.py
@@ -1,17 +1,49 @@
-number_1 = input("Enter the first number: ")
-number_2 = input("Enter the second number: ")
-operator = input("Enter the operator: ")
+def add (n1, n2):
+    return n1 + n2
 
-match operator:
-    case '+':
-        final_value = int(number_1) + int(number_2)
-        print(final_value)
-    case '-':
-        final_value = int(number_1) - int(number_2)
-        print(final_value)
-    case '*':
-        final_value = int(number_1) * int(number_2)
-        print(final_value)
-    case '/':
-        final_value = int(number_1) / int(number_2)
-        print(final_value)
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add, 
+    "-": subtract, 
+    "*": multiply, 
+    "/": divide
+}
+
+def calculate():
+    should_accumulate = True
+    num1 = float(input("What is your first number? "))
+
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input('Pick an operation: ')
+        num2 = float(input("What is your second number? "))
+
+        answer = operations[operation_symbol](num1, num2)
+
+        print(f'{answer}')
+
+        choice = input(f"Type 'yes' to continue calculating with {answer}, or type 'no' to start a new calculation.")
+
+        if choice == 'yes':
+            num1 = answer
+        else:
+            should_accumulate = False
+            print(f"Your final result is: {answer}")
+
+def main():
+    try:
+        calculate()
+    except ValueError:
+        print("Something went wrong!")
+
+if __name__ == "__main__":
+    main()
